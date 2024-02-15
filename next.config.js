067a68485814fd9559ffa188f385c44f11349087
korsak Sridech <korsak.sridech@gmail.com>
2024-02-15 22:46:28 +0700

    update v3

--- next.config.js
+++ next.config.js
@@ -1,18 +1,47 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV !== "development",
-  },
-};
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   compiler: {
+//     removeConsole: process.env.NODE_ENV !== "development",
+//   },
+// };
 
-const withPWA = require("next-pwa")({
+// const withPWA = require("next-pwa")({
+//   dest: "public",
+//   // disable: process.env.NODE_ENV === "development",
+//   // register: true,
+//   // skipWaiting: true,
+// });
+
+// module.exports = withPWA(nextConfig);
+
+
+// const {
+//   PHASE_DEVELOPMENT_SERVER,
+//   PHASE_PRODUCTION_BUILD,
+// } = require("next/constants");
+
+// /** @type {import("next").NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// module.exports = (phase) => {
+//   if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+//     const withPWA = require("@ducanh2912/next-pwa").default({
+//       dest: "public",
+//     });
+//     return withPWA(nextConfig);
+//   }
+//   return nextConfig;
+// };
+
+const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
-  // disable: process.env.NODE_ENV === "development",
-  // register: true,
-  // skipWaiting: true,
 });
 
-module.exports = withPWA(nextConfig);
+module.exports = withPWA({
+  // Your Next.js config
+});
