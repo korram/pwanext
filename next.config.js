851613ab2ea5d586ac4fc2ff07d1de7d3641b38c
korsak Sridech <korsak.sridech@gmail.com>
2024-02-15 22:37:33 +0700

    update  v2

--- next.config.js
+++ next.config.js
@@ -10,9 +10,9 @@ const nextConfig = {
 
 const withPWA = require("next-pwa")({
   dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
+  // disable: process.env.NODE_ENV === "development",
+  // register: true,
+  // skipWaiting: true,
 });
 
 module.exports = withPWA(nextConfig);
